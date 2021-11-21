c8e7ebc4346353a735350dea6924cf63807b73f0
plector <kenanpoole@gmail.com>
2021-11-21 10:50:42 -0500

    Beta conversation working

--- script.js
+++ script.js
@@ -41,22 +41,50 @@ function findBigram(word, bigramList){
 }
 
 function generateLineFromTrigram(given){
-    
+    var newLine = "";
     for (let i = 0; i < 16; i++) {
         var wordList = findBigram(given[1], findFirstEntry(given[0]))  
         var nextWord;
+
         if(wordList.length != 0){
             nextWord = wordList[Math.floor(Math.random() * wordList.length)];
         }else{
             nextWord = trigrams[Math.floor(Math.random() * trigrams.length)][1];
         }
-
+        newLine = newLine + " " + nextWord;
         console.log(nextWord);
 
         given[0] = given[1];
         given[1] = nextWord;
     }
 
+    return newLine;
+
     //console.log(findBigram("away",findFirstEntry("passed")));
 
 }
+
+function generateLine(sent){
+    console.log(sent);
+    var ending = sent.split(" ").splice(-2);
+    if(ending.length<2){
+        alert("Write a litter more!")
+        return;
+    }else{
+        document.getElementById("meInput").value = "";
+        document.getElementById("text-cont").innerHTML += "<div class='text'><span class='me'>Me</span>" + " " + sent +"</div> ";
+        console.log(ending);
+        
+        var janeLine = generateLineFromTrigram(ending);
+        //wait(7000);
+        document.getElementById("text-cont").innerHTML += "<div class='text'><span class='jane'>Jane Austen</span>" + " " + janeLine +"</div> ";
+    }
+}
+
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
